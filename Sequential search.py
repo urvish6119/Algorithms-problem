0ea5d4a9d13361d48dc630b50368adcc01054b7c
Urvish P. Patel <urvish132712@gmail.com>
2018-01-27 21:01:15 -0600

    Update Sequential search.py

--- Sequential search.py
+++ Sequential search.py
@@ -1,40 +1,40 @@
 # Unordered list:
-def sequential_search(array, element):
+def sequential_search(list1, element):
     position = 0
     found = False
-    while position < len(array) and not found:
-        if array[position] == element:
+    while position < len(list1) and not found:
+        if list1[position] == element:
             found = True
         else:
             position += 1
     print(found)
 print('enter array elements here with space')
-list1 = [int(a) for a in input().split()]
+list_i = [int(a) for a in input().split()]
 x = int(input('please enter number you want to find.'))
-sequential_search(list1,x)
+sequential_search(list_i,x)
 
 # Ordered list:
 
-def sequential_search(array, element):
+def sequential_search(list1, element):
     position = 0
     found = False
     stop = False
-    while position < len(array) and not found and not stop:
-        if array[position] == element:
+    while position < len(list1) and not found and not stop:
+        if list1[position] == element:
             found = True
         else:
-            if array[position] > element:
+            if list1[position] > element:
                 stop = True
             else:
                 position += 1
     print(found)
 print('enter array elements here with space')
-list1 = [int(a) for a in input().split()]
-list1.sort()
+list_i = [int(a) for a in input().split()]
+list_i.sort()
 
 temp = []
 new_list = []
-for i in list1:
+for i in list_i:
     if i not in temp:
         temp.append(i)
         new_list.append(i)
